7bf5f1ac39fce3fb0393ac5d7a60b863d184db4e
Nicholas Voyagis <nvoyagis@calpoly.edu>
2024-04-23 09:40:07 -0700

    updated audit

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -16,16 +16,12 @@ router = APIRouter(
 def get_inventory():
     """ """
     with db.engine.begin() as connection:
-        num_red_pots = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
-        num_green_pots = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
-        num_blue_pots = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
-        num_of_pots = num_red_pots + num_green_pots + num_blue_pots
-        num_red_ml = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory")).scalar_one()
-        num_green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar_one()
-        num_blue_ml = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).scalar_one()
-        num_of_ml = num_red_ml + num_green_ml + num_blue_ml
-        num_of_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory")).scalar_one()
-    return {"number_of_potions": num_of_pots, "ml_in_barrels": num_of_ml, "gold": num_of_gold}
+        potion_list = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock")).all()
+        num_of_pots = potion_list[0] + potion_list[1] + potion_list[2] + potion_list[3] + potion_list[4] + potion_list[5]
+        ml_list = connection.execute(sqlalchemy.text("SELECT * FROM resources")).all()
+        total_ml = ml_list[0] + ml_list[1] + ml_list[2]
+        num_of_gold = connection.execute(sqlalchemy.text("SELECT gold FROM extra_resources")).scalar_one()
+    return {"number_of_potions": num_of_pots, "ml_in_barrels": total_ml, "gold": num_of_gold}
 
 # Gets called once a day
 @router.post("/plan")
