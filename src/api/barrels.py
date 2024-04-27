d9700094db7240d28a91cf9c98000d1c7beb0029
Nicholas Voyagis <nvoyagis@calpoly.edu>
2024-04-26 19:52:36 -0700

    small bugfix

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -33,10 +33,10 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 
         # Combine ledgers and remove them
         with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("UPDATE resources SET red_ml = (SELECT SUM(red_change) FROM resources_ledgers) + resources.red_ml"))
-            connection.execute(sqlalchemy.text("UPDATE resources SET green_ml = (SELECT SUM(green_change) FROM resources_ledgers) + resources.green_ml"))
-            connection.execute(sqlalchemy.text("UPDATE resources SET blue_ml = (SELECT SUM(blue_change) FROM resources_ledgers) + resources.blue_ml"))
-            connection.execute(sqlalchemy.text("UPDATE resources SET gold = (SELECT SUM(gold_change) FROM resources_ledgers) + resources.gold"))
+            connection.execute(sqlalchemy.text("UPDATE resources SET red_ml = (SELECT SUM(red_change) FROM resource_ledgers) + resources.red_ml"))
+            connection.execute(sqlalchemy.text("UPDATE resources SET green_ml = (SELECT SUM(green_change) FROM resource_ledgers) + resources.green_ml"))
+            connection.execute(sqlalchemy.text("UPDATE resources SET blue_ml = (SELECT SUM(blue_change) FROM resource_ledgers) + resources.blue_ml"))
+            connection.execute(sqlalchemy.text("UPDATE resources SET gold = (SELECT SUM(gold_change) FROM resource_ledgers) + resources.gold"))
             connection.execute(sqlalchemy.text("TRUNCATE resources"))
 
 
@@ -59,7 +59,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         for barrel in wholesale_catalog:
             r = random.randint(0,2)
             if barrel.price <= gold and r == 0 and barrel.potion_type == [1, 0, 0, 0]: 
-                connection.execute(sqlalchemy.text("INSERT INTO resources_ledgers (red_change, green_change, blue_change, gold_change) VALUES (:red, :green, :blue, :gold)"), [{"red": barrel.ml_per_barrel, "blue": 0, "green": 0, "gold": -barrel.price}])
+                connection.execute(sqlalchemy.text("INSERT INTO resource_ledgers (red_change, green_change, blue_change, gold_change) VALUES (:red, :green, :blue, :gold)"), [{"red": barrel.ml_per_barrel, "blue": 0, "green": 0, "gold": -barrel.price}])
                 gold -= barrel.price
                 json.append({
                     "sku": barrel.sku,
@@ -67,7 +67,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                 })
                 
             elif barrel.price <= gold and r == 1 and barrel.potion_type == [0, 1, 0, 0]: 
-                connection.execute(sqlalchemy.text("INSERT INTO resources_ledgers (red_change, green_change, blue_change, gold_change) VALUES (:red, :green, :blue, :gold)"), [{"red": 0, "blue": barrel.ml_per_barrel, "green": 0, "gold": -barrel.price}])
+                connection.execute(sqlalchemy.text("INSERT INTO resource_ledgers (red_change, green_change, blue_change, gold_change) VALUES (:red, :green, :blue, :gold)"), [{"red": 0, "blue": barrel.ml_per_barrel, "green": 0, "gold": -barrel.price}])
                 gold -= barrel.price
                 json.append({
                     "sku": barrel.sku,
@@ -75,7 +75,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                 })
 
             elif barrel.price <= gold and r == 2 and barrel.potion_type == [0, 0, 1, 0]: 
-                connection.execute(sqlalchemy.text("INSERT INTO resources_ledgers (red_change, green_change, blue_change, gold_change) VALUES (:red, :green, :blue, :gold)"), [{"red": 0, "blue": 0, "green": barrel.ml_per_barrel, "gold": -barrel.price}])
+                connection.execute(sqlalchemy.text("INSERT INTO resource_ledgers (red_change, green_change, blue_change, gold_change) VALUES (:red, :green, :blue, :gold)"), [{"red": 0, "blue": 0, "green": barrel.ml_per_barrel, "gold": -barrel.price}])
                 gold -= barrel.price
                 json.append({
                     "sku": barrel.sku,
