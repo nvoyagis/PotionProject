ca28b91e3d3579a5c6426a873e1a183ffb21af41
Nicholas Voyagis <nvoyagis@calpoly.edu>
2024-04-29 14:03:10 -0700

    changed bottlers

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -20,26 +20,35 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     """ """
     with db.engine.begin() as connection:
         red_sum = connection.execute(sqlalchemy.text("SELECT SUM(red_change) FROM potion_ledgers")).scalar_one()
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.red_ml - 100 * :sum"), [{"sum": red_sum}])
         red_cur = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock where id = 1")).scalar_one()
         connection.execute(sqlalchemy.text("UPDATE potion_stock SET quantity = :new_red WHERE id = 1"), [{"new_red": red_sum + red_cur}])
 
         green_sum = connection.execute(sqlalchemy.text("SELECT SUM(green_change) FROM potion_ledgers")).scalar_one()
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.green_ml - 100 * :sum"), [{"sum": green_sum}])
         green_cur = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock where id = 2")).scalar_one()
         connection.execute(sqlalchemy.text("UPDATE potion_stock SET quantity = :new_green WHERE id = 2"), [{"new_green": green_sum + green_cur}])
 
         blue_sum = connection.execute(sqlalchemy.text("SELECT SUM(blue_change) FROM potion_ledgers")).scalar_one()
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.blue_ml - 100 * :sum"), [{"sum": blue_sum}])
         blue_cur = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock where id = 3")).scalar_one()
         connection.execute(sqlalchemy.text("UPDATE potion_stock SET quantity = :new_blue WHERE id = 3"), [{"new_blue": blue_sum + blue_cur}])
 
         brown_sum = connection.execute(sqlalchemy.text("SELECT SUM(brown_change) FROM potion_ledgers")).scalar_one()
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.red_ml - 50 * :sum"), [{"sum": brown_sum}])
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.green_ml - 50 * :sum"), [{"sum": brown_sum}])
         brown_cur = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock where id = 4")).scalar_one()
         connection.execute(sqlalchemy.text("UPDATE potion_stock SET quantity = :new_brown WHERE id = 4"), [{"new_brown": brown_sum + brown_cur}])
 
         teal_sum = connection.execute(sqlalchemy.text("SELECT SUM(teal_change) FROM potion_ledgers")).scalar_one()
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.green_ml - 50 * :sum"), [{"sum": teal_sum}])
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.blue_ml - 50 * :sum"), [{"sum": teal_sum}])
         teal_cur = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock where id = 5")).scalar_one()
         connection.execute(sqlalchemy.text("UPDATE potion_stock SET quantity = :new_teal WHERE id = 5"), [{"new_teal": teal_sum + teal_cur}])
 
         purple_sum = connection.execute(sqlalchemy.text("SELECT SUM(purple_change) FROM potion_ledgers")).scalar_one()
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.red_ml - 50 * :sum"), [{"sum": purple_sum}])
+        connection.execute(sqlalchemy.text("UPDATE resources SET quantity = resources.blue_ml - 50 * :sum"), [{"sum": purple_sum}])
         purple_cur = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_stock where id = 6")).scalar_one()
         connection.execute(sqlalchemy.text("UPDATE potion_stock SET quantity = :new_purple WHERE id = 6"), [{"new_purple": purple_sum + purple_cur}])
 
